75a820504400d1c44d23ee0f305d0105a9dc55f1
taliamartheze <71811244+taliamartheze@users.noreply.github.com>
2022-03-14 14:09:00 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,20 @@
+
+
+    <footer class="container-fluid text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <a class="navbar-brand" href="#"><span class="josh">JOSH</span><span
+                            class="orange">WHITKIN</span></a>
+
+                    <div class="col-md-6">
+                        <p>Terms and conditions</p>
+                    </div>
+
+                </div><!-- row -->
+            </div><!-- container  -->
+    </footer>
+    <?php get_footer();?>   <!-- Tells WordPress to include footer.php   -->
+</body>
+
+</html>
