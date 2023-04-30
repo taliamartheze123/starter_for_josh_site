5f626a82e54238a5fb83e5198d66740bd06e53df
taliamartheze <71811244+taliamartheze@users.noreply.github.com>
2023-04-30 19:45:45 +0800

    Create home-page.php

---
+++ home-page.php
@@ -0,0 +1,70 @@
+<?php
+    /*
+    Template Name: Home page
+    */
+    ?>    
+<?php get_header();?>   <!--  Tells WordPress to include header.php -->
+
+    <section class="container-fluid aboutbg text-center">
+        <div class="container">
+
+            <div class="blocker"></div>
+
+        </div>
+    </section>
+
+    <section class="container-fluid articlebg">
+        <div class="container">
+            <h3 class="text-center">LATEST ARTICLES</h3>
+        </div><!-- CONTAINER -->
+
+        <div class="row">
+            <div class="col-md-4">
+                <img class="img-fluid" src="http://170.187.231.66/~mesh14/ wp-content/themes/starter_for_josh_site/images/portrait.jpg">
+                <h4 class="article-title">A PORTRAIT</h4>
+                <p class="date">Friday March 10. 2022</p>
+                <p class="article-p">STS has never been shy of seeking new terrains of investigation. More and more STS
+                    scholars are
+                    sr=tarting to explore and invervence in the arts. This object of study brings new challenges and
+                    opportunities that we want to explore in this session. We would like to gather first of all simply
+                    new kinds of knowledge arising from STS study of the arts.</p>
+
+                <a class="readmore" href="" #>CONTINUE READING</a>
+            </div>
+
+            <div class="col-md-4">
+                <img class="img-fluid" src="
+
+                ftp://170.187.231.66/~mesh14/">
+
+                <h4 class="article-title">A PORTRAIT</h4>
+                <p class="date">Friday March 10. 2022</p>
+                <p class="article-p">STS has never been shy of seeking new terrains of investigation. More and more STS
+                    scholars are
+                    sr=tarting to explore and invervence in the arts. This object of study brings new challenges and
+                    opportunities that we want to explore in this session. We would like to gather first of all simply
+                    new kinds of knowledge arising from STS study of the arts.</p>
+
+                <a class="readmore" href="" #>CONTINUE READING</a>
+            </div>
+
+            <div class="col-md-4">
+                <img class="img-fluid" src="
+
+                ftp://170.187.231.66/~mesh14/">
+
+                <h4>A PORTRAIT</h4>
+                <p>Friday March 10. 2022</p>
+                <p>STS has never been shy of seeking new terrains of investigation. More and more STS scholars are
+                    sr=tarting to explore and invervence in the arts. This object of study brings new challenges and
+                    opportunities that we want to explore in this session. We would like to gather first of all simply
+                    new kinds of knowledge arising from STS study of the arts.</p>
+
+                <a class="readmore" href="" #>CONTINUE READING</a>
+
+            </div>
+        </div><!-- row -->
+
+    </section>
+    <?php get_footer();?>   <!-- Tells WordPress to include footer.php   -->
+   
