7188b2815f52a44779cdd3c0d56aeff748b5aa44
taliamartheze <71811244+taliamartheze@users.noreply.github.com>
2023-04-30 20:39:18 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -8,6 +8,8 @@
     <section class="container-fluid aboutbg text-center">
         <div class="container">
         <h2><?php the_field('titleforaboutme'); ?></h2><!--  custom field for the aboutme content-->
+        <p class="particle"><?php the_field('subtitleforaboutme'); ?></p>
+<p class="about-text"><?php the_field('paragraphforaboutme'); ?></p>
 
             <div class="blocker"></div>
 
