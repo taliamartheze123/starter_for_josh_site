3fcc9503d34adc7573d8c7032ab0861ea5c7981b
taliamartheze <71811244+taliamartheze@users.noreply.github.com>
2023-04-30 21:48:34 +0800

    Update header.php

--- header.php
+++ header.php
@@ -40,7 +40,7 @@
 
 <body>
 
-    <header class="container-fluid bg1">
+    <header class="container-fluid smallpage-bg">
         <div class="container">
 
             <nav class="navbar navbar-expand-md navbar-dark">
