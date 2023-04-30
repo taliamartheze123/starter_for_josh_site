472ae54cd4930f052614fefe27f10e8bb5125ba7
taliamartheze <71811244+taliamartheze@users.noreply.github.com>
2023-04-30 21:18:59 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -18,21 +18,17 @@
 
     <section class="container-fluid articlebg">
         <div class="container">
-            <h3 class="text-center">LATEST ARTICLES</h3>
+            <h3 class="text-center">"><?php the_field('titleforarticles'); ?></h3></h3>
         </div><!-- CONTAINER -->
 
         <div class="row">
             <div class="col-md-4">
-                <img class="img-fluid" src="http://170.187.231.66/~mesh14/wp-content/themes/starter_for_josh_site/images/portrait.jpg">
-                <h4 class="article-title">A PORTRAIT</h4>
-                <p class="date">Friday March 10. 2022</p>
-                <p class="article-p">STS has never been shy of seeking new terrains of investigation. More and more STS
-                    scholars are
-                    sr=tarting to explore and invervence in the arts. This obje  ct of study brings new challenges and
-                    opportunities that we want to explore in this session. We would like to gather first of all simply
-                    new kinds of knowledge arising from STS study of the arts.</p>
+            <img class="img-fluid" alt=“portait” src="<?php the_field('articleimage');?>">
+            <h4 class="article-title"><?php the_field('articletitle'); ?></h4>
+            <p class="date" ><?php the_field('articledate'); ?></p>
+            <p class="article-p"><?php the_field('articleexcerpt'); ?> </p>
 
-                <a class="readmore" href="" #>CONTINUE READING</a>
+            <a href="<?php the_field('readmore'); ?>" class="readmore">CONTINUE READING</a>
             </div>
 
             <div class="col-md-4">
