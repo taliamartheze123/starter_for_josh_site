50d49cc6212eb4d1d3220c1f6e202dc209f039b3
taliamartheze <71811244+taliamartheze@users.noreply.github.com>
2023-04-30 21:42:40 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -5,7 +5,7 @@
     ?>    
 <?php get_header();?>   <!--  Tells WordPress to include header.php -->
 
-<?php require_once('page_header.php'); ?>
+
 
     <section class="container-fluid aboutbg text-center">
         <div class="container">
