47bc9c11638edae5be4fe2da926ec8522de6e9d2
taliamartheze <71811244+taliamartheze@users.noreply.github.com>
2023-05-01 22:52:42 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,8 +4,9 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-6">
-                    <a class="navbar-brand" href="#"><span class="josh">JOSH</span><span
-                            class="orange">WHITKIN</span></a>
+                   <!-- <a class="navbar-brand" href="#"><span class="josh">JOSH</span><span class="orange">WHITKIN</span></a> -->
+                   <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-left') ) : 
+endif; ?>
 
                     <div class="col-md-6">
                         <p>Terms and conditions</p>
