8594f2a80e7412c8d167eb09b50f47c403f86acc
taliamartheze <71811244+taliamartheze@users.noreply.github.com>
2023-05-01 22:28:23 +0800

    Update page_header.php

--- page_header.php
+++ page_header.php
@@ -40,7 +40,7 @@
 
 <body>
 
-    <header class="container-fluid smallpage-bg">
+<header class="container-fluid" style="background-size:cover; height:40vh; background-image: url(<?php the_field('projectheaderbackgroundimage') ?>);">
         <div class="container">
 
             <nav class="navbar navbar-expand-md navbar-dark">
@@ -52,27 +52,24 @@
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
-                <!-- Navbar links -->
-                <div class="collapse navbar-collapse" id="collapsibleNavbar">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">HOME</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">PROJECTS</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">CV</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">CONTACT</a>
-                        </li>
-                    </ul>
-                </div>
+                <?php 
+
+wp_nav_menu( array(
+  'theme_location'  => 'primary',
+  'depth'           => 2, // 1 = no dropdowns, 2 = with dropdowns.
+  'container'       => 'div',
+  'container_class' => 'collapse navbar-collapse',
+  'container_id'    => 'bs-example-navbar-collapse-1',
+  'menu_class'      => 'navbar-nav ml-auto',
+  'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+  'walker'          => new WP_Bootstrap_Navwalker(),
+) );
+
+?>
             </nav>
 
             <div class="smallpage-tag"></div>
-            <h1 class="smallpage-title">Projects</h1>
+            <h1 class="smallpage-title">"><?php the_field('projectsheadertitle'); ?> </h1>
         </div>
         </div> <!-- container -->
 
